1e82ab881234049c5f5cab1ef0009960a3feeff4
Karolina Jimenez G <karojg@gmail.com>
2023-03-22 18:32:29 -0600

    states main functionality

--- intermediate/us_states_game/us-states-game-start/main.py
+++ intermediate/us_states_game/us-states-game-start/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -12,6 +13,29 @@ turtle.shape(image)
 #     print(x,y)
 # turtle.onscreenclick(get_mouse_click_coordinate)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name")
+STATES_COUNT = 50
+data = pandas.read_csv("50_states.csv")
+all_states = data["state"].to_list()
+guessed_states = []
+
+
+while len(guessed_states) < STATES_COUNT:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/{STATES_COUNT} States Correct",
+                                    prompt="What's another state's name").title()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+
+        # get the coordinate
+        state_coordinate = data[data["state"] == answer_state]
+        t.goto(int(state_coordinate.x), int(state_coordinate.y))
+        t.write(answer_state)
+
+        # using series.item()
+        # t.write(state_coordinate.state.item())
+
 turtle.mainloop()
 
