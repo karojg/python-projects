13081dd3e56afd07bc7128cbc9e72575f7b1984c
Karolina Jimenez G <karojg@gmail.com>
2023-03-23 09:46:44 -0600

    states with list_comprehension

--- intermediate/us_states_game/main.py
+++ intermediate/us_states_game/main.py
@@ -24,10 +24,11 @@ while len(guessed_states) < STATES_COUNT:
                                     prompt="What's another state's name")
 
     if answer_state == "Exit" or answer_state is None:
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        #     missing_states = []
+        #     for state in all_states:
+        #         if state not in guessed_states:
+        #             missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
