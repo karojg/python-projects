accd2d23e8f59b9e5ab1e566b1b21920013eadaa
Karolina Jimenez G <karojg@gmail.com>
2023-03-22 21:41:07 -0600

    completed functionality

--- intermediate/us_states_game/us-states-game-start/main.py
+++ intermediate/us_states_game/us-states-game-start/main.py
@@ -21,7 +21,16 @@ guessed_states = []
 
 while len(guessed_states) < STATES_COUNT:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/{STATES_COUNT} States Correct",
-                                    prompt="What's another state's name").title()
+                                    prompt="What's another state's name")
+
+    if answer_state == "Exit" or answer_state is None:
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
     if answer_state in all_states:
         guessed_states.append(answer_state)
